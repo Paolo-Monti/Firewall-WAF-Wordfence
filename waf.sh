602b9fa79272638948c1f8ef145979f159909701
Paolo-Monti <48802688+Paolo-Monti@users.noreply.github.com>
2019-03-24 18:13:54 +0100

    Update waf.sh

--- waf.sh
+++ waf.sh
@@ -13,8 +13,7 @@ declare -a sites=("/var/www/vhosts/blog" "/var/www/vhosts/portal" "/var/www/vhos
 ##### BEGIN OPTIONAL #####
 ini="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"/waf.ini
 [ -f "$ini" ] && chain=$(sed -e 's|\s*$||' -nre 's|\s*chain\s*=\s*(.*)|\1|p' "$ini")
-[ -z "$chain" ] && chain=Wordfence
-iptables -F "$chain" &>/dev/null
+iptables -F "${chain:-Wordfence}" &>/dev/null
 #####  END OPTIONAL #####
 
 # Loop to check the Wordpress sites included inside the array declared above
